7a428c82b6c7ef0637f816b2e385815381204440
siddhikhalate <siddhi.khalate2000@gmail.com>
2026-06-08 12:52:24 +0530

    Add new Vercel URL to CORS

--- Backend/lakeSide-hotel/src/main/java/com/lakeSideHotel/security/CorsConfig.java
+++ Backend/lakeSide-hotel/src/main/java/com/lakeSideHotel/security/CorsConfig.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import java.util.List;
 import java.util.List;
 
-
 @Configuration
 public class CorsConfig {
 
@@ -24,17 +23,17 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = 
             new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
             "http://localhost:5173",
-            "https://hotel-management-eight-livid.vercel.app"
+            "https://hotel-management-eight-livid.vercel.app",
+            "https://hotel-management-2-seven.vercel.app"
         ));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
-        
+        config.setAllowedMethods(List.of(
+            "GET","POST","PUT","DELETE","OPTIONS"
+        ));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
-
